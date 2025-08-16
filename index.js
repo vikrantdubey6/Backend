b6bf5649d14ce87a22aa9db11341d2ae85c44a6f
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-08-16 17:33:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+
 
 app.get( '/', (req, res) => {
   res.send('Hello World!')
@@ -14,7 +16,7 @@ app.get('/login', (req, walk)=> {
     walk.send('<h1> please login</h1>')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
     console.log(`example app listening at ${port}`);
     
 })
