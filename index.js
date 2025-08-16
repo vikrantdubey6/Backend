82acdfa68e338a52c57a7d6c48cb388e8cd4594d
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-08-16 16:30:11 +0530

    making server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get( '/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/about', (req, res)=>{
+    res.send("i m vikrant dubey and i m learning node js")
+})
+
+app.get('/login', (req, walk)=> {
+    walk.send('<h1> please login</h1>')
+})
+
+app.listen(port, () => {
+    console.log(`example app listening at ${port}`);
+    
+})
