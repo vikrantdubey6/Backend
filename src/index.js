773104bc8e42b14f4cf14ccb2a58fe94d8982666
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-08-22 00:20:17 +0530

    file setup

---
+++ src/index.js
@@ -0,0 +1,70 @@
+// require('dotenv').config
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+// when we called function connectDB it will return a promise because we are using aync await in that Fn()
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed!!", err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ( async() => {
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error", (error) => {
+//         console.log("ERR ", error);
+//         throw error
+//        } )
+
+//        app.listen(process.env.PORT, () => {
+//         console.log(`App is listening on port ${process.env.PORT}`)
+//        })
+
+
+//     } catch (error) {
+//         console.error("ERROR", error)
+//         throw error
+//     }
+// }   )()
