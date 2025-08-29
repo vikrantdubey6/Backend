392f8abbe6f2f4683a0db56336ca125c56160c99
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-08-30 01:30:20 +0530

    user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -128,28 +128,62 @@ const loginUser = asyncHandler(async (req, res) => {
   const isPasswordCorrect = await user.isPasswordCorrect(password);
   if (!isPasswordCorrect) {
     throw new ApiError(401, "Invalid password");
-}
+  }
 
- const {accessToken, refreshToken}  = await generateAccessAndRefreshToken(user._id)
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
 
-    const loggedInUser = await User.findById(User._id).select("-password -refreshToken")
+  const loggedInUser = await User.findById(User._id).select(
+    "-password -refreshToken"
+  );
 
-    const options = {
-        httpOnly: true,
-        secure: true
-    }
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
 
-    return res.status(200)
+  return res
+    .status(200)
     .cookie("accessToken", accessToken, options)
     .cookie("refreshToken", refreshToken, options)
     .json(
-        new ApiResponse(
-            200,{
-                user: loggedInUser, accessToken, refreshToken
-            },
-            "User logged in successfully"
-        )
-    )
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged in successfully"
+      )
+    );
 });
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(req.user._id, {
+    $set: {
+      refreshToken: undefined,
+    },
+  },
+  {
+    new: true
+  }
+);
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+  .status(200)
+  .clearCookie("accessToken", options)
+  .clearCookie("refreshToken", options)
+  .json(
+    new ApiResponse(200, {}, "User logged out successfully!!!")
+  )
+
+});
+
+export { registerUser, loginUser, logoutUser };
